b19e1bf255b2b930f95a966d990227167b826efd
WangXueqing007 <wangxq0124@gmail.com>
2017-02-22 09:45:31 +0100

    Add files via upload

---
+++ organize_project.sh
@@ -0,0 +1,18 @@
+mkdir "$1"
+cd "$1"
+mkdir project
+cd project
+mkdir ./scripts
+mkdir ./bash
+mkdir ./bash/runall.sh
+mkdir ./bash/filter*sh
+mkdir ./input
+mkdir ./output
+mkdir ./logs
+>readme.txt
+>commands.txt
+
+cd ..
+mkdir datasets
+mkdir general_scripts
+mkdir bin
